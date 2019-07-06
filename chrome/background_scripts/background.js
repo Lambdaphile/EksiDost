d286b0d9e48c411dc90edaeb5f62608f700ea85e
genesis-algorithms <genesisalgorithms@gmail.com>
2019-07-06 14:35:12 +0300

    fix(chrome): Bouncy color palette elements...
    Since Chrome extension API doesn't have "browser_style" option
    palette elements were 'bouncy' when hovered...

---
+++ chrome/background_scripts/background.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/extensions */
+/* eslint-disable no-undef */
+import {
+  getActiveTab, injectCSS, setCookies, onError,
+} from '../modules/module.js';
+
+function handlePageReload() {
+  getActiveTab().then((tabs) => {
+    /* getting previously set cookies */
+    const gettingCookies = browser.cookies.get({
+      url: tabs[0].url,
+      name: 'favourite-color',
+    });
+
+    gettingCookies.then((cookie) => {
+      /* if there are availible cookies - insert styles */
+      if (cookie) {
+        const cookieCSS = JSON.parse(cookie.value);
+        injectCSS(cookieCSS);
+      } else {
+        /* default styles for fresh installs... */
+        const defaultCSS = '.topic-list a:visited {color: purple;}';
+        injectCSS(defaultCSS);
+        setCookies(tabs[0].url, 'favourite-color', defaultCSS);
+      }
+    });
+  });
+}
+
+const filter = {
+  urls: ['*://eksisozluk.com/*'],
+};
+
+/* update when the tab is updated */
+browser.tabs.onUpdated.addListener(handlePageReload, filter);
---
+++ chrome/popup/scripts/color-palette.js
@@ -0,0 +1,57 @@
+/* eslint-disable import/named */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import {
+  getActiveTab, injectCSS, ejectCSS, setCookies, onError,
+} from '../../modules/module.js';
+
+const colorPalette = document.querySelectorAll('.color-palette li');
+const customColorInput = document.getElementById('custom-color-input');
+const customColorSubmit = document.querySelector('.submit');
+
+function setColor(event) {
+  getActiveTab().then((tabs) => {
+    /* getting selected color value */
+    const paletteElement = getComputedStyle(event.target);
+    const css = `.topic-list a:visited {
+      color: ${paletteElement.backgroundColor};
+    }`;
+
+    /* remove previous set styles */
+    ejectCSS(css);
+
+    /* insert new styles */
+    injectCSS(css);
+
+    /* Setting cookies */
+    setCookies(tabs[0].url, 'favourite-color', css);
+  });
+}
+
+colorPalette.forEach((color) => {
+  color.addEventListener('click', setColor, false);
+});
+
+customColorSubmit.addEventListener('click', () => {
+  getActiveTab().then((tabs) => {
+    const customCSS = `.topic-list a:visited {
+      color: ${customColorInput.value};
+    }`;
+
+    ejectCSS(customCSS);
+    injectCSS(customCSS);
+
+    setCookies(tabs[0].url, 'favourite-color', customCSS);
+  });
+});
+
+browser.cookies.onChanged.addListener((changeInfo) => {
+  console.log(
+    `Cookie changed:\n
+        * Cookie: ${JSON.stringify(changeInfo.cookie)}\n
+        * Cause: ${changeInfo.cause}\n
+        * Removed: ${changeInfo.removed}`,
+  );
+});
