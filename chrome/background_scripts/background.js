898e06d6bfa6ef00dffcb68fc2519831b5bde3a5
genesis-algorithms <genesisalgorithms@gmail.com>
2019-07-22 23:49:39 +0300

    refactor(chrome): Make setStyles() anonymous function, Improve naming.

--- chrome/background_scripts/background.js
+++ chrome/background_scripts/background.js
@@ -4,37 +4,32 @@ import {
   getActiveTab, injectCSS, setCookies,
 } from '../modules/module.js';
 
-function setStyles(tabs) {
-  /* getting previously set cookies */
-  chrome.cookies.get({
-    url: tabs[0].url,
-    name: 'favourite-color',
-  },
-  (cookie) => {
-    /* if there are availible cookies - insert styles */
-    if (cookie) {
-      let style = JSON.parse(cookie.value);
-      /* restoring css code */
-      style += ';}';
-
-      injectCSS(style);
-    } else {
-      /* default styles for fresh installs... */
-      const defaultStyle = '.topic-list a:visited {color: purple;}';
-
-      injectCSS(defaultStyle);
-
-      setCookies(tabs[0].url, 'favourite-color', defaultStyle);
-    }
-  });
-}
-
-function validateURL() {
+function cookieUpdate() {
+  /* getting previously set styles from cookies */
   getActiveTab((tabs) => {
-    if (tabs[0].url !== 'undefined') setStyles(tabs);
+    chrome.cookies.get({
+      url: tabs[0].url,
+      name: 'favourite-color',
+    },
+    (cookie) => {
+      /* if there are availible cookies - insert styles */
+      if (cookie) {
+        let storedStyle = JSON.parse(cookie.value);
+        /* restoring css code... */
+        storedStyle += ';}';
+
+        injectCSS(storedStyle);
+      } else {
+        /* default styles for fresh installs... */
+        const defaultStyle = '.topic-list a:visited {color: purple;}';
+        injectCSS(defaultStyle);
+
+        setCookies(tabs[0].url, 'favourite-color', defaultStyle);
+      }
+    });
   });
 }
 
-/* update when the tab is updated */
-chrome.tabs.onUpdated.addListener(validateURL);
-chrome.tabs.onActivated.addListener(validateURL);
+/* listen for tab activations and updates */
+chrome.tabs.onUpdated.addListener(cookieUpdate);
+chrome.tabs.onActivated.addListener(cookieUpdate);
--- chrome/popup/scripts/color-palette.js
+++ chrome/popup/scripts/color-palette.js
@@ -9,27 +9,25 @@ const colorPalette = document.querySelectorAll('.color-palette li');
 const customColorInput = document.getElementById('custom-color-input');
 const customColorSubmit = document.querySelector('.submit');
 
-function setStyles(event) {
-  getActiveTab((tabs) => {
-    /* getting selected color value */
-    const colorPaletteElement = getComputedStyle(event.target);
-    const style = `.topic-list a:visited {
-      color: ${colorPaletteElement.backgroundColor};
-    }`;
-    /* insert new styles */
-    injectCSS(style);
-
-    /* setting cookies */
-    setCookies(tabs[0].url, 'favourite-color', style);
-  });
-}
-
 colorPalette.forEach((color) => {
-  color.addEventListener('click', setStyles, false);
+  color.addEventListener('click', (event) => { 
+    getActiveTab((tabs) => {
+      /* take selected color value and create the css code */
+      const colorPaletteElement = getComputedStyle(event.target);
+      const style = `.topic-list a:visited {
+        color: ${colorPaletteElement.backgroundColor};
+      }`;
+      /* insert new styles */
+      injectCSS(style);
+
+      /* setting cookies... */
+      setCookies(tabs[0].url, 'favourite-color', style);
+    });
+  }, false);
 });
 
 customColorSubmit.addEventListener('click', () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  getActiveTab((tabs) => {
     const customStyle = `.topic-list a:visited {
       color: ${customColorInput.value};
     }`;
@@ -44,8 +42,8 @@ customColorSubmit.addEventListener('click', () => {
 chrome.cookies.onChanged.addListener((changeInfo) => {
   console.log(
     `Cookie changed:\n
-        * Cookie: ${JSON.stringify(changeInfo.cookie)}\n
-        * Cause: ${changeInfo.cause}\n
-        * Removed: ${changeInfo.removed}`,
+      * Cookie: ${JSON.stringify(changeInfo.cookie)}\n
+      * Cause: ${changeInfo.cause}\n
+      * Removed: ${changeInfo.removed}`,
   );
 });
